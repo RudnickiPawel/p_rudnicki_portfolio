2326ebde4d7e4a43dd90b6060f285ec5aafef428
RudnickiPawel <rudnicki.pawel94@gmail.com>
2021-11-26 14:19:06 +0100

    project cards update

--- src/components/Main/Section/ProjectCard/ProjectCard.js
+++ src/components/Main/Section/ProjectCard/ProjectCard.js
@@ -2,18 +2,24 @@ import '../../../../styles/main.css';
 
 const ProjectCard = (props) => {
   return (
-    <div className='ProjectCard'>
-      <div className='ProjectCard__side ProjectCard__front'>
-        <img src={props.img} alt='' />
-        <h1>{props.title}</h1>
-        <h2>{props.content}</h2>
+    <div className='ProjectCard__wrapper'>
+      <div className='ProjectCard'>
+        <div className='ProjectCard__side ProjectCard__front'>
+          <h1>{props.title}</h1>
+          <img src={props.img} alt='' />
+          <h2>{props.content}</h2>
+        </div>
+        <div href={props.url} className='ProjectCard__side ProjectCard__back'>
+          <h1>Użyto:</h1>
+          {props.techs.map((tech, index) =>
+            <p key={index}>{tech}</p>
+          )}
+        </div>
       </div>
-      <a href={props.url} className='ProjectCard__side ProjectCard__back'>
-        <h1>Użyto:</h1>
-        {props.techs.map((tech, index) => 
-        <p key={index}>{tech}</p>
-        )}
-      </a>
+      <div className="ProjectCard__btns-wrapper">
+        <a className='ProjectCard__button' href={props.url}>Strona</a>
+        <a className='ProjectCard__button' href={props.github}>Kod</a>
+      </div>
     </div>
   );
 };
