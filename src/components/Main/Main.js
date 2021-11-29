d147b2996e97bfd14a9a0012f08503ad5595266d
RudnickiPawel <rudnicki.pawel94@gmail.com>
2021-11-29 15:46:51 +0100

    skills section update

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -17,7 +17,7 @@ const Main = (props) => {
       if (entries[0].isIntersecting === true) {
         selectedFlyingText = Array.from(document.querySelectorAll('.Section__item--flying'));
         selectedFlyingText.map(text => {
-          if(text.classList.contains('Section__item1'))
+          if (text.classList.contains('Section__item1'))
             text.style.color = 'rgba(248,170,103,255)';
           text.style.left = '0px'
           return null
@@ -54,40 +54,38 @@ const Main = (props) => {
       </Section>
       <Section id='skills' className='skills'>
         <h1 className='Section__title'>Umiejętności</h1>
-        <div className='Section__background'>
-          <div className="Section__container">
-            <div className="Section__item">
-              <FontAwesomeIcon className='icon' icon={faHtml5} />
-              <h3>html5</h3>
-            </div>
-            <div className="Section__item">
-              <FontAwesomeIcon className='icon' icon={faCss3} />
-              <h3>css3</h3>
-            </div>
-            <div className="Section__item">
-              <FontAwesomeIcon className='icon' icon={faJs} />
-              <h3>javascript <br /> es6</h3>
-            </div>
-            <div className="Section__item">
-              <FontAwesomeIcon className='icon' icon={faNpm} />
-              <h3>npm</h3>
-            </div>
-            <div className="Section__item">
-              <FontAwesomeIcon className='icon' icon={faReact} />
-              <h3>react</h3>
-            </div>
-            <div className="Section__item">
-              <FontAwesomeIcon className='icon' icon={faNode} />
-              <h3>node.js</h3>
-            </div>
-            <div className="Section__item">
-              <FontAwesomeIcon className='icon' icon={faGit} />
-              <h3>git</h3>
-            </div>
-            <div className="Section__item">
-              <FontAwesomeIcon className='icon' icon={faSass} />
-              <h3>sass</h3>
-            </div>
+        <div className="Section__container">
+          <div className="Section__item">
+            <FontAwesomeIcon className='icon' icon={faHtml5} />
+            <h3>html5</h3>
+          </div>
+          <div className="Section__item">
+            <FontAwesomeIcon className='icon' icon={faCss3} />
+            <h3>css3</h3>
+          </div>
+          <div className="Section__item">
+            <FontAwesomeIcon className='icon' icon={faJs} />
+            <h3>javascript <br /> es6</h3>
+          </div>
+          <div className="Section__item">
+            <FontAwesomeIcon className='icon' icon={faNpm} />
+            <h3>npm</h3>
+          </div>
+          <div className="Section__item">
+            <FontAwesomeIcon className='icon' icon={faReact} />
+            <h3>react</h3>
+          </div>
+          <div className="Section__item">
+            <FontAwesomeIcon className='icon' icon={faNode} />
+            <h3>node.js</h3>
+          </div>
+          <div className="Section__item">
+            <FontAwesomeIcon className='icon' icon={faGit} />
+            <h3>git</h3>
+          </div>
+          <div className="Section__item">
+            <FontAwesomeIcon className='icon' icon={faSass} />
+            <h3>sass</h3>
           </div>
         </div>
         <Arrow targetId='#about' version='black' />
